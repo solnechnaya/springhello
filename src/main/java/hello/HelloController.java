9f2e688354f628a99eefe77a85b020c3b3552392
Oleg Z <utmons@yandex.com>
2017-06-29 11:23:01 +0600

    удален статический контент

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class StaticController {
+@RestController
+public class HelloController {
 
     @RequestMapping("/")
     public String index() {
--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class StaticControllerTest {
+public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
